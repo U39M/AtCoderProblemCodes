e505f45f7001024c10f812facaeafbb737880b72
U39M <40987478+U39M@users.noreply.github.com>
2023-04-01 22:54:18 +0900

    ✨ abc296の追加 (#21)

---
+++ abc/abc296/296_A.py
@@ -0,0 +1,8 @@
+if __name__ == "__main__":
+    n = int(input())
+    s = input()
+
+    if "MM" not in s and "FF" not in s:
+        print("Yes")
+    else:
+        print("No")
---
+++ abc/abc296/ReadMe.md
@@ -0,0 +1,9 @@
+# AtCoder Beginner Contest 296
+
+リンク:[https://atcoder.jp/contests/abc296]
+
+A問題　クリア
+B問題　クリア
+C問題　TLE
+
+forの多重ループは計算量が多くなるのでNG
