1901d9f1dadd8eb3872e4bcd7785c6d3979b74a1
Yusaku Matsuo <40987478+U39M@users.noreply.github.com>
2025-05-10 01:40:36 +0900

    ✨ templateの更新 (#25)

--- c++practice/template/template.cpp
+++ c++practice/template/template.cpp
@@ -1,7 +1,59 @@
-#include <bits/stdc++.h>
-using namespace std;
+# include <cassert>
+# include <cctype>
+# include <cmath>
+# include <cstdio>
+# include <cstdlib>
+# include <algorithm>
+# include <bitset>
+# include <deque>
+# include <functional>
+# include <iostream>
+# include <iomanip>
+# include <limits>
+# include <list>
+# include <map>
+# include <numeric>
+# include <queue>
+# include <set>
+# include <sstream>
+# include <stack>
+# include <string>
+# include <tuple>
+# include <utility>
+# include <vector>
 
-int main()
-{
- //   cout << "Hello, world!" << endl;
+using namespace std;
+using ll = long long int;
+using ld = long double;
+
+template<typename T> using v = vector<T>;
+template<typename T> using vv = vector<vector<T>>;
+template<typename T> using vvv = vector<vector<vector<T>>>;
+template<typename T> inline void errv(T& v) { for (auto x: v) cerr << x << " "; cerr << endl; }
+inline void errv(vector<bool>& v) { for (auto x: v) cerr << (x ? 1 : 0) << " "; cerr << endl; }
+template<typename T> inline void dbgn(string n, T x) { cerr << n << ": " << x << endl; }
+template<typename T> inline void dbgn(string n, vector<T>& v) { cerr << n << ": "; errv(v); }
+template<typename T> inline void dbgn(string n, vector<vector<T>>& m) { cerr << n << ":" << endl; for (auto& v: m) errv(v); }
+
+# define _GLIBCXX_DEBUG
+# define rep(i, n) for (ll i = 0; i < (n); ++i)
+# define repp(i, c, n) for (ll i = c; i < (n); ++i)
+# define repa(i, a) for (auto i: a)
+# define all(a) (a).begin(),(a).end()
+# define rall(a) (a).rbegin(),(a).rend()
+# define so(v) sort((v).begin(), (v).end())
+# define rso(v) sort((v).rbegin(), (v).rend())
+# define len(x) ll((x).size())
+
+//p(出力) d(デバッグ *多次元配列も可能*)
+# define p(x) cout << x << endl;
+# define d(x) dbgn(#x, x);
+
+const ll INF = 1LL << 60;  //無限大
+const ll MOD = 1000000007; //10^9 + 7
+
+int main(){
+
+    return 0;
 }
+
